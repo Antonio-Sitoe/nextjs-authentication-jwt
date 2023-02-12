a42dd64dcea98708847890bdb6221083ea6056ec
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2023-02-12 12:54:03 +0200

    add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,9 @@
 import { AppProps } from "next/app";
-import { Header } from "../components/Header";
-import { Provider as NextAuthProvider } from "next-auth/client";
 
-import "../styles/global.scss";
+import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <NextAuthProvider session={pageProps.session}>
-      <Header />
-      <Component {...pageProps} />
-    </NextAuthProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,97 @@
-import Head from "next/head";
-import styles from "./Home.module.scss";
-
-import { SubscribeButton } from "../components/SubscribeButton";
-import { GetStaticProps } from "next";
-import { stripe } from "../services/Stripe";
-
-interface HomeProps {
-  product: {
-    priceId: string;
-    amount: string;
-  };
-}
-// Conteudo o mesmo para todas as pessoas.
-export const getStaticProps: GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve(process.env.PRODUCT_API_ID); // ID DO PRODUCTO QUE VOCE CADASTROU LA NO DASBOARD DO STRIPE
-  const product = {
-    priceId: price.id,
-    amount: new Intl.NumberFormat("pt-MZ", {
-      currency: "MZN",
-    }).format(price.unit_amount / 100),
-  };
-  return {
-    props: {
-      product,
-    },
-    revalidate: 60 * 60 * 24, // 24 hours ou 1 dia
-  };
-};
-
-export default function Home({ product }: HomeProps) {
+export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Home |Ignews</title>
-      </Head>
-      <main className={styles.contentContainer}>
-        <section className={styles.hero}>
-          <span>👏, Seja bem vindo</span>
-          <h1>
-            Novidades Sobre o universo do <span>React.Js </span>
-          </h1>
-          <p>
-            Saiba mais sobre o ecossistema Javascript <br />
-            <span>por apenas {product.amount} MZN por mes</span>
-          </p>
-          {/* <SubscribeButton /> */}
-        </section>
-        <img src="/avatar.svg" alt="girl coding" />
-      </main>
-    </>
+    <div className="w-full h-screen flex items-center justify-center border-1">
+      <div className="">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            {/* <img
+              className="mx-auto h-12 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            /> */}
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              Or{" "}
+              <a
+                href="#"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                start your 14-day free trial
+              </a>
+            </p>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
